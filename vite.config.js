94d327252d599681fc6ac2d85b3b419653d9a297
ASTUT0 <noahlezama2000@gmail.com>
2024-10-05 02:46:18 +0200

    Refactor package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Alias para la carpeta src
-    },
-  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/images/*",
+          dest: "assets/images", // Carpeta destino en `dist`
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: "dist",
   },

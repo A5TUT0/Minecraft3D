1b85689c9cf055e6b7d447fe488a2cbe79164065
ASTUT0 <noahlezama2000@gmail.com>
2024-10-05 02:41:18 +0200

    Refactor texture imports and exports

--- src/Images/textures.js
+++ src/Images/textures.js
@@ -16,33 +16,16 @@ const dirtTexture = new TextureLoader().load(dirtImg);
 const logTexture = new TextureLoader().load(logImg);
 const glassTexture = new TextureLoader().load(glassImg);
 const woodTexture = new TextureLoader().load(woodImg);
-const obsidianTexture = new TextureLoader().load(obsidianImg); // Nueva textura
-const stoneTexture = new TextureLoader().load(stoneImg); // Nueva textura
+const obsidianTexture = new TextureLoader().load(obsidianImg);
+const stoneTexture = new TextureLoader().load(stoneImg);
 
-// Crear y configurar groundTexture (utilizando grassTexture como base)
-const groundTexture = new TextureLoader().load(grassImg); // Puedes cambiar a otra textura si prefieres
-
-// Configurar wrapping para repetir la textura en un plano grande
-groundTexture.wrapS = RepeatWrapping;
-groundTexture.wrapT = RepeatWrapping;
-groundTexture.magFilter = NearestFilter;
-
-// Configurar otras texturas
-grassTexture.magFilter = NearestFilter;
-dirtTexture.magFilter = NearestFilter;
-logTexture.magFilter = NearestFilter;
-glassTexture.magFilter = NearestFilter;
-woodTexture.magFilter = NearestFilter;
-obsidianTexture.magFilter = NearestFilter;
-stoneTexture.magFilter = NearestFilter;
-
+// Asegúrate de que todas las texturas se exporten
 export {
-  groundTexture, // Exportar groundTexture
   grassTexture,
   dirtTexture,
   logTexture,
   glassTexture,
   woodTexture,
-  obsidianTexture, // Nueva textura
-  stoneTexture, // Nueva textura
+  obsidianTexture,
+  stoneTexture,
 };
